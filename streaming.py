5836ce51f913f0728a838e35c5acfde4b4174036
Your Name <you@example.com>
2023-08-01 16:24:07 +0200

    clear

---
+++ streaming.py
@@ -0,0 +1,20 @@
+""" require ffmpeg"""
+#sudo apt install v4l-utils
+#v4l2-ctl --list-devices
+
+class webstream:
+    pass
+
+
+class Instagram:
+    pass
+
+
+class Facebook:
+    pass
+
+class Twitch:
+    pass
+
+class youtube:
+    pass
